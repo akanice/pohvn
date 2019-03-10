914d245f144a06d54f275098b388b568bddfa27a
nghiennet89@gmail.com <nghiennet89@gmail.com>
2019-03-10 15:59:47 +0700

    short code

--- application/controllers/news.php
+++ application/controllers/news.php
@@ -1,135 +1,162 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
-class News extends MY_Controller{
+class News extends MY_Controller {
     private $data;
+
     function __construct() {
         parent::__construct();
         $this->load->library('auth');
-		$this->auth = new Auth();
+        $this->auth = new Auth();
         if ($this->auth->isUserLogin()) {
-			$this->data['affiliate_user'] = $this->auth->getUser();
-			$this->load->model('usersmodel');
-			$this->data['user_profile'] = $this->usersmodel->read(array('id'=>$this->data['affiliate_user']['id']),array(),true);
-		}
-		//Get Menu 
+            $this->data['affiliate_user'] = $this->auth->getUser();
+            $this->load->model('usersmodel');
+            $this->data['user_profile'] = $this->usersmodel->read(array('id' => $this->data['affiliate_user']['id']), array(), true);
+        }
+        //Get Menu
         $this->load->model('menusmodel');
-		
-		$nav_data = $this->menusmodel->read(array('menu_id'=>'1'));
-		$this->data['navmenu'] = json_decode(json_encode($nav_data), true);
-		$this->data['footermenu'] = $this->menusmodel->read(array('menu_id'=>2));
-		$this->data['config_navmenu'] = $this->menusmodel->setup_navmenu();
-		$this->data['config_mobilemenu'] = $this->menusmodel->setup_mobilemenu();
-		
-		//print_r($this->data['config_navmenu']);die();
+
+        $nav_data = $this->menusmodel->read(array('menu_id' => '1'));
+        $this->data['navmenu'] = json_decode(json_encode($nav_data), true);
+        $this->data['footermenu'] = $this->menusmodel->read(array('menu_id' => 2));
+        $this->data['config_navmenu'] = $this->menusmodel->setup_navmenu();
+        $this->data['config_mobilemenu'] = $this->menusmodel->setup_mobilemenu();
+
+        //print_r($this->data['config_navmenu']);die();
         $this->load->model('menustermmodel');
         $this->load->model('configsmodel');
-        
+
         //Options
-		$this->load->model('optionsmodel');
-		$options = array_swap_index($this->optionsmodel->read(), 'name');
+        $this->load->model('optionsmodel');
+        $options = array_swap_index($this->optionsmodel->read(), 'name');
         $this->data['options'] = $options;
-		$this->data['home_logo']								= @$options['home_logo']->value;
-        $this->data['tour_banner'] 								= @$options['tour_banner']->value;
-        $this->data['home_hotline']							= @$options['home_hotline']->value;
-        $this->data['home_short_introduction'] 		= @$options['home_short_introduction']->value;
-        $this->data['link_facebook'] 							= @$options['link_facebook']->value;
-        $this->data['link_twitter'] 								= @$options['link_twitter']->value;
-        $this->data['link_gplus'] 									= @$options['link_gplus']->value;
-        $this->data['link_instagram'] 							= @$options['link_instagram']->value;
-        $this->data['tour_banner'] 								= @$options['tour_banner']->value;
-		
-		$this->load->model('newsmodel');
-		$this->load->model('newscategorymodel');
+        $this->data['home_logo'] = @$options['home_logo']->value;
+        $this->data['tour_banner'] = @$options['tour_banner']->value;
+        $this->data['home_hotline'] = @$options['home_hotline']->value;
+        $this->data['home_short_introduction'] = @$options['home_short_introduction']->value;
+        $this->data['link_facebook'] = @$options['link_facebook']->value;
+        $this->data['link_twitter'] = @$options['link_twitter']->value;
+        $this->data['link_gplus'] = @$options['link_gplus']->value;
+        $this->data['link_instagram'] = @$options['link_instagram']->value;
+        $this->data['tour_banner'] = @$options['tour_banner']->value;
+
+        $this->load->model('newsmodel');
+        $this->load->model('newscategorymodel');
     }
-    public function index($alias){
-		$this->add_count($alias);
-		// load data
-		$this->data['new'] = $this->newsmodel->read(array('alias'=>$alias),array(),true);
-		if (isset($this->data['new']) && ($this->data['new'] != '')) {
-			$new_id = $this->data['new']->id;
-			
-			if ($this->data['new']->type == 'default') {
-				$this->data['title'] = $this->data['new']->title;
-				$categoryid = json_decode($this->data['new']->categoryid);
-				
-				foreach ($categoryid as $n=>$value) {
-					$this->data['category'][$n] = $cat_data = $this->newscategorymodel->read(array('id'=>$value),array(),true); 
-					if ($cat_data->parent_id==null or $cat_data->parent_id==0)  {$cat_chosen  = $value;}
-				}
-				$this->load->model('adminsmodel');
-				$author_id = $this->data['new']->author_id;
-				$this->data['author'] = $this->adminsmodel->read(array('id'=>$author_id),array(),true);
-				$this->data['most_viewed'] = $this->newsmodel->read(array(),array('count_view'=>false),false,5);
-				$this->data['related_news'] = $this->newsmodel->getRelatedNews($cat_chosen,5);
-				$this->load->view('home/common/header',  $this->data);
-				$this->load->view('home/news_detail');
-				$this->load->view('home/common/footer');
-			} else {
-				$this->load->model('landingpagemodel');
-				$this->data['title'] = $this->data['new']->title;
-				$this->data['cookies_expires'] = $this->configsmodel->read(array('term'=>'affiliate','name'=>'cookie_time'),array(),true)->value/(24*60*60);
-				$this->data['landing_data'] = $this->landingpagemodel->read(array('news_id'=>$new_id),array(),true);
-				$this->load->view('home/common/header_landing',  $this->data);
-				$this->load->view('home/template/landing_page');
-				$this->load->view('home/common/footer_landing');
-			}
-		} else {
-			redirect('404_override');
-		}
+
+    public function index($alias) {
+
+        $arrShortCode = array(
+            '[banner_top_display]' => 'banner_top',
+            '[banner_bottom_display]' => 'banner_bottom'
+        );
+
+        $this->add_count($alias);
+        // load data
+        $this->data['new'] = $this->newsmodel->read(array('alias' => $alias), array(), true);
+        $content = $this->data['new']->content;
+        //TODO - do short code here
+        foreach($arrShortCode as $shortCodeStr => $funcName) {
+            $shortCodePos = strrpos($content, $shortCodeStr);
+            if($shortCodePos !== false) {
+                do {
+                    $contentBefore = substr($content, 0, $shortCodePos);
+                    $contentShortCode = $this->$funcName();
+                    $contentAfter = substr($content, $shortCodePos + strlen($shortCodeStr));
+                    $content = $contentBefore.$contentShortCode.$contentAfter;
+                    $shortCodePos = strrpos($content, $shortCodeStr);
+                } while ($shortCodePos !== false);
+            }
+        }
+        $this->data['new']->content = $content;
+        if (isset($this->data['new']) && ($this->data['new'] != '')) {
+            $new_id = $this->data['new']->id;
+
+            if ($this->data['new']->type == 'default') {
+                $this->data['title'] = $this->data['new']->title;
+                $categoryid = json_decode($this->data['new']->categoryid);
+
+                foreach ($categoryid as $n => $value) {
+                    $this->data['category'][$n] = $cat_data = $this->newscategorymodel->read(array('id' => $value), array(), true);
+                    if ($cat_data->parent_id == null or $cat_data->parent_id == 0) {
+                        $cat_chosen = $value;
+                    }
+                }
+                $this->load->model('adminsmodel');
+                $author_id = $this->data['new']->author_id;
+                $this->data['author'] = $this->adminsmodel->read(array('id' => $author_id), array(), true);
+                $this->data['most_viewed'] = $this->newsmodel->read(array(), array('count_view' => false), false, 5);
+                $this->data['related_news'] = $this->newsmodel->getRelatedNews($cat_chosen, 5);
+                $this->load->view('home/common/header', $this->data);
+                $this->load->view('home/news_detail');
+                $this->load->view('home/common/footer');
+            } else {
+                $this->load->model('landingpagemodel');
+                $this->data['title'] = $this->data['new']->title;
+                $this->data['cookies_expires'] = $this->configsmodel->read(array(
+                        'term' => 'affiliate',
+                        'name' => 'cookie_time'), array(), true)->value / (24 * 60 * 60);
+                $this->data['landing_data'] = $this->landingpagemodel->read(array('news_id' => $new_id), array(), true);
+                $this->load->view('home/common/header_landing', $this->data);
+                $this->load->view('home/template/landing_page');
+                $this->load->view('home/common/footer_landing');
+            }
+        } else {
+            redirect('404_override');
+        }
     }
-	
-	public function category($alias) {
-        $this->data['news_category'] = $news_category = $this->newscategorymodel->read(array('alias'=>$alias),array(),true);
-		$total = $this->newsmodel->readCountNew($news_category->id);
-		$per_page = 6;
-		$this->configPagination($slug='category',$per_page,$alias,$total);
+
+    function add_count($alias) {
+        // load cookie helper
+        $this->load->helper('cookie');
+        // this line will return the cookie which has alias name
+        $check_visitor = $this->input->cookie(urldecode($alias), FALSE);
+        // this line will return the visitor ip address
+        $ip = $this->input->ip_address();
+        // if the visitor visit this article for first time then //
+        //set new cookie and update article_views column  ..
+        //you might be notice we used alias for cookie name and ip
+        //address for value to distinguish between articles  views
+        if ($check_visitor == false) {
+            $cookie = array(
+                "name"   => urldecode($alias),
+                "value"  => "$ip",
+                "expire" => time() + 7200,
+                "secure" => false
+            );
+            $this->input->set_cookie($cookie);
+            $this->newsmodel->update_counter(urldecode($alias));
+        }
+    }
+
+    public function category($alias) {
+        $this->data['news_category'] = $news_category = $this->newscategorymodel->read(array('alias' => $alias), array(), true);
+        $total = $this->newsmodel->readCountNew($news_category->id);
+        $per_page = 6;
+        $this->configPagination($slug = 'category', $per_page, $alias, $total);
         $page_number = $this->uri->segment(3);
         if (empty($page_number)) $page_number = 1;
         $start = ($page_number - 1) * $per_page;
         $this->data['page_links'] = $this->pagination->create_links();
-        $this->data['news'] = $this->newsmodel->getListNews('',$news_category->id,$per_page,$start);
+        $this->data['news'] = $this->newsmodel->getListNews('', $news_category->id, $per_page, $start);
         if (empty($news_category->title)) {
-			$this->data['title'] = 'Chuyên mục'	;
-		} else {
-			$this->data['title'] = 'Chuyên mục- '.$news_category->title;
-		}
+            $this->data['title'] = 'Chuyên mục';
+        } else {
+            $this->data['title'] = 'Chuyên mục- ' . $news_category->title;
+        }
 
-		$this->data['most_viewed'] = $this->newsmodel->read(array(),array('count_view'=>false),false,5);
-				
-		$this->data['meta_keywords']    = $news_category->meta_keywords;
-		$this->data['meta_description']	= $news_category->meta_description;
-		
-		$this->load->view('home/common/header',$this->data);
+        $this->data['most_viewed'] = $this->newsmodel->read(array(), array('count_view' => false), false, 5);
+
+        $this->data['meta_keywords'] = $news_category->meta_keywords;
+        $this->data['meta_description'] = $news_category->meta_description;
+
+        $this->load->view('home/common/header', $this->data);
         $this->load->view('home/news_list');
         $this->load->view('home/common/footer');
-	}
-
-	public function news_search() {
-		//$this->data['prod_cat'] = $this->productcategorymodel->read();
-		$this->data['name'] = $this->input->get('s_keyword');
-		$total = $this->newsmodel->getCountNew($this->data['name'],'','','');
-		$per_page = 6;
-		if($this->data['name'] != ""){
-            $config['suffix'] = '?keyword='.urlencode($this->data['name']);
-        }
-        //Pagination
-        $this->configPagination($slug='search',$per_page,$alias='page',$total);
-        $page_number = $this->uri->segment(3);
-        if (empty($page_number)) $page_number = 1;
-        $start = ($page_number - 1) * $per_page;
-        $this->data['page_links'] = $this->pagination->create_links();
-        $this->data['result'] = $this->newsmodel->getNewsSearch($this->data['name'],'',$per_page,$start);
-		//print_r($this->data['result']);die();
-		$this->data['title']    = 'Search: '.$this->input->get('s_keyword');
-		
-		$this->load->view('home/common/header',$this->data);
-        $this->load->view('home/news_search');
-        $this->load->view('home/common/footer');
     }
-	
-	private function configPagination($slug,$per_page=9,$alias,$total) {
+
+    private function configPagination($slug, $per_page = 9, $alias, $total) {
         $this->load->library('pagination');
-        $config['base_url'] = base_url().$slug.'/'.$alias;
+        $config['base_url'] = base_url() . $slug . '/' . $alias;
         $config['total_rows'] = $total;
         $config['uri_segment'] = 3;
         $config['per_page'] = $per_page;
@@ -151,30 +178,37 @@ class News extends MY_Controller{
         $config["prev_link"] = "← Trước";
         $config["prev_tag_open"] = "<li class='prev'>";
         $config["prev_tag_close"] = "</li>";
-		$config['attributes'] = array('class' => 'page-link');
+        $config['attributes'] = array('class' => 'page-link');
         $this->pagination->initialize($config);
-	}
-	
-	function add_count($alias) {
-		// load cookie helper
-		$this->load->helper('cookie');
-		// this line will return the cookie which has alias name
-		$check_visitor = $this->input->cookie(urldecode($alias), FALSE);
-		// this line will return the visitor ip address
-		$ip = $this->input->ip_address();
-		// if the visitor visit this article for first time then //
-		//set new cookie and update article_views column  ..
-		//you might be notice we used alias for cookie name and ip 
-		//address for value to distinguish between articles  views
-		if ($check_visitor == false) {
-			$cookie = array(
-				"name"   => urldecode($alias),
-				"value"  => "$ip",
-				"expire" =>  time() + 7200,
-				"secure" => false
-			);
-			$this->input->set_cookie($cookie);
-			$this->newsmodel->update_counter(urldecode($alias));
-		}
-	}
+    }
+
+    public function news_search() {
+        //$this->data['prod_cat'] = $this->productcategorymodel->read();
+        $this->data['name'] = $this->input->get('s_keyword');
+        $total = $this->newsmodel->getCountNew($this->data['name'], '', '', '');
+        $per_page = 6;
+        if ($this->data['name'] != "") {
+            $config['suffix'] = '?keyword=' . urlencode($this->data['name']);
+        }
+        //Pagination
+        $this->configPagination($slug = 'search', $per_page, $alias = 'page', $total);
+        $page_number = $this->uri->segment(3);
+        if (empty($page_number)) $page_number = 1;
+        $start = ($page_number - 1) * $per_page;
+        $this->data['page_links'] = $this->pagination->create_links();
+        $this->data['result'] = $this->newsmodel->getNewsSearch($this->data['name'], '', $per_page, $start);
+        //print_r($this->data['result']);die();
+        $this->data['title'] = 'Search: ' . $this->input->get('s_keyword');
+
+        $this->load->view('home/common/header', $this->data);
+        $this->load->view('home/news_search');
+        $this->load->view('home/common/footer');
+    }
+
+    private function banner_bottom() {
+        return '-------banner bottom---------';
+    }
+    private function banner_top() {
+        return '-------banner top---------';
+    }
 }
